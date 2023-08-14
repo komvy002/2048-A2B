472bb98b5c3b36c8837bdb1b1321ac291f6f5322
Viktor <72117821+s3882570@users.noreply.github.com>
2023-08-14 22:25:48 +0800

    Refactored File Access into a separate file

--- word_statistics_app.py
+++ word_statistics_app.py
@@ -1,43 +1,6 @@
-import os
 import click
 from typing import Protocol, List, Dict
-
-# ========== File Access ==========
-
-# Define the File Accessor Interface
-class FileAccessorProtocol(Protocol):
-    def read(self, path: str) -> str:
-        ...
-
-    def write(self, path: str, content: str, mode: str = 'w') -> None:
-        ...
-# Implement Default File Accessor
-class DefaultFileAccessor:
-    def read(self, path: str) -> str:
-        with open(path, 'r') as file:
-            return file.read()
-
-    def write(self, path: str, content: str, mode: str = 'w') -> None:
-        with open(path, mode) as file:
-            file.write(content)
-
-class OutputPathValidator:
-    VALID_FORMATS = ['.csv', '.txt']
-
-    @staticmethod
-    def validate(_ctx, _param, paths) -> Dict[str, str]:
-        def getPathFormat(path):
-            _base, extension = os.path.splitext(path)
-            return extension.lower()
-
-        output_spec = {}
-        for path in paths:
-            format = getPathFormat(path)
-            if format not in OutputPathValidator.VALID_FORMATS:
-                raise click.BadParameter(f"Output file {path} has an invalid format {format}. Known formats are {', '.join(OutputPathValidator.VALID_FORMATS)}.")
-            output_spec[path] = format
-
-        return output_spec
+from file_access_module import FileAccessorProtocol, DefaultFileAccessor, OutputPathValidator
 
 # ========== Tokeniser ==========
 
