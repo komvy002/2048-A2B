6c3e514d3f8d41946ac148bd9d8854130dc12916
Viktor <72117821+s3882570@users.noreply.github.com>
2023-08-20 21:05:11 +0800

    Test for invalid file formats

--- test_file_access_module.py
+++ test_file_access_module.py
@@ -1,3 +1,4 @@
+import pytest
 from file_access_module import DefaultFileAccessor, OutputPathValidator, click
 
 # ================== Tests for file_access_module ==================
@@ -42,10 +43,17 @@ def test_append_file(tmp_path):
 
 def test_for_valid_file_format():
     # Test for valid file formats
-    output_spec = OutputPathValidator.validate(None, None, ["in.csv", "in.txt"])
+    output_spec = OutputPathValidator.validate(None, None, ["out.csv", "out.txt"])
 
     expected_output_spec = {
-        "in.csv": ".csv",
-        "in.txt": ".txt"
+        "out.csv": ".csv",
+        "out.txt": ".txt"
     }
     assert output_spec == expected_output_spec
+    
+def test_for_invalid_file_format():
+    # Test for an invalid file format
+    with pytest.raises(click.BadParameter) as exc_info:
+        OutputPathValidator.validate(None, None, ["out.doc"])
+
+    assert "has an invalid format .doc" in str(exc_info.value)
