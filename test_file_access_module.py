48a9d39ef2fa98fc1baece75567e326940022c1e
Viktor <72117821+s3882570@users.noreply.github.com>
2023-08-20 14:12:45 +0800

    Complete file reading test

--- test_file_access_module.py
+++ test_file_access_module.py
@@ -1,17 +1,15 @@
-import pytest
 from file_access_module import DefaultFileAccessor, OutputPathValidator, click
 
-# ================== Tests for DefaultFileAccessor ==================
+# ================== Tests for file_access_module ==================
 
-def test_read_file(tmp_path):
-    # Setup
+# ========== Test for DefaultFileAccessor ==========
+
+def test_reading_file(tmp_path):
     file_content = "Greetings Program!"
     test_file = tmp_path / "test.txt"
     test_file.write_text(file_content)
     accessor = DefaultFileAccessor()
-
-    # Exercise
+    
     read_content = accessor.read(test_file)
-
-    # Verify
     assert read_content == file_content
+
